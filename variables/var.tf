608db1c62dde1b894f0d15758bb51997385f06c8
sriram <munukutla2001@gmail.com>
2023-10-27 08:03:56 +0530

    project

--- variables/var.tf
+++ variables/var.tf
@@ -5,3 +5,14 @@ variable "sample" {
 output "sample2" {
   value = var.sample
 }
+
+
+
+###########list type variables
+variable "list" {
+  default =["AWS","DEVOPS","CLOUD"]
+}
+
+output "list_output" {
+  value = var.list
+}

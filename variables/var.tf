479027ede2ca429f009262fe76634eaa39fdc3c7
sriram <munukutla2001@gmail.com>
2023-10-27 08:10:02 +0530

    project

--- variables/var.tf
+++ variables/var.tf
@@ -16,3 +16,22 @@ variable "list" {
 output "list_output" {
   value = var.list[2]
 }
+
+
+########### map type variables
+variable "map" {
+  default = {
+    devops = {
+      Time= "11AM"
+      Duration= "100days"
+    }
+    AWS={
+      Time= "12AM"
+      Duration= "40days"
+    }
+  }
+}
+
+output "map_output" {
+  value = var.map
+}

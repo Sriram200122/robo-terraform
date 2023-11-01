84f1338d04c1d7bd29b30be5555c3e3381c23a38
sriram <munukutla2001@gmail.com>
2023-11-01 11:23:06 +0530

    project

--- demo/sample.tf
+++ demo/sample.tf
@@ -3,15 +3,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Sriram"
-  }
-}
-
-resource "aws_instance" "web" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t2.micro"
-
-  tags = {
     Name = "Man"
   }
 }
+

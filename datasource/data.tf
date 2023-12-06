f4ebac2419a50262ccf281da3d4f09a6ff7cfb19
sriram <munukutla2001@gmail.com>
2023-12-06 11:56:58 +0530

    project

---
+++ datasource/data.tf
@@ -0,0 +1,23 @@
+data "aws_ec2_spot_price" "example" {
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+
+  filter {
+    name   = "product-description"
+    values = ["Linux/UNIX"]
+  }
+}
+
+output "t2_micro_spot_price" {
+  value = data.aws_ec2_spot_price.example.spot_price
+}
+
+###### aws security group datasource terraform  ######
+data "aws_security_group" "selected" {
+  id = "sg-06ddc4457f334b890"
+}
+
+output "allow_all_id" {
+  value = data.aws_security_group.selected.vpc_id
+}
+
